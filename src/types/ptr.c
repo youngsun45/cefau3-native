91cd55398b8bd013aa4e967dd6cf7da954288a8c
Duy .Ng <38210249+wy3@users.noreply.github.com>
2018-08-30 07:14:10 +0700

    Add files via upload

--- src/types/ptr.c
+++ src/types/ptr.c
@@ -1,31 +1,31 @@
 #include "../cefau3.h"
 
-void CEFCALLBACK IntPtr_Set(int *self, int value)
+void __stdcall IntPtr_Set(int *self, int value)
 {
 	*self = value;
 }
 
-int CEFCALLBACK IntPtr_Get(int *self)
+int __stdcall IntPtr_Get(int *self)
 {
 	return *self;
 }
 
-void CEFCALLBACK LongPtr_Set(long *self, long value)
+void __stdcall LongPtr_Set(long *self, long value)
 {
 	*self = value;
 }
 
-long CEFCALLBACK LongPtr_Get(long *self)
+long __stdcall LongPtr_Get(long *self)
 {
 	return *self;
 }
 
-void CEFCALLBACK DoublePtr_Set(double *self, double value)
+void __stdcall DoublePtr_Set(double *self, double value)
 {
 	*self = value;
 }
 
-double CEFCALLBACK DoublePtr_Get(double *self)
+double __stdcall DoublePtr_Get(double *self)
 {
 	return *self;
 }
